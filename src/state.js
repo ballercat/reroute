86a1d11de26dcb53ed14f45dbdd971efb5ad1d18
Arty Buldauskas <abuldauskas@wayfair.com>
2017-07-01 17:53:58 -0400

    prettier stuff

--- src/state.js
+++ src/state.js
@@ -1,28 +1,24 @@
-import Immutable from 'seamless-immutable';
+import Immutable from "seamless-immutable";
 
-const __INITIAL__ = '__INITIAL__';
+const __INITIAL__ = "__INITIAL__";
 // Date.now() is not sufficient for some scenarios when the same time can be
 // generated for two states, id and tiem created are kept separate.
 let __CONTEXT_COUNTER__ = 0;
 
 export const copy = (state, meta = { id: __CONTEXT_COUNTER__++ }) =>
-  state.merge(
-    meta,
-    {
-      timestamp: Date.now()
-    }
-  );
+  state.merge(meta, { timestamp: Date.now() });
 
-const state = (meta = {
-  path: __INITIAL__
-}, data = {}) =>
+const state = (
+  meta = {
+    path: __INITIAL__
+  },
+  data = {}
+) =>
   Immutable({
     ...meta,
     data,
-    id: (__CONTEXT_COUNTER__++),
+    id: __CONTEXT_COUNTER__++,
     timestamp: Date.now()
   });
 
-
 export default state;
-
